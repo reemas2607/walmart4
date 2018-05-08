cb378e9d73f42d0edf0344c0ae81cecc73737ad0
reemas2607 <39100292+reemas2607@users.noreply.github.com>
2018-05-09 01:00:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.walmart.com/ip/Teqnio-10-1-Ultra-Slim-Laptop-Windows-10-Intel-Atom-Processor-2GB-memory-32GB-storage-Front-Facing-Camera-6-hour-battery-Available-assorted-colors/56123855");
 die();
 ?>
